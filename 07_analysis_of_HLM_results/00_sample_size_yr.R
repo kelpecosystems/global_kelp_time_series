20ccc1db7d2330daadc894154e598b8c1c31f495
Jarrett Byrnes <jarrett.byrnes@umb.edu>
2016-01-25 12:04:37 -0500

    Updating plotting code

--- 07_analysis_of_HLM_results/00_sample_size_yr.R
+++ 07_analysis_of_HLM_results/00_sample_size_yr.R
@@ -1,17 +1,18 @@
 ###Simple code to plot number of sampling points by year###
 library(dplyr)
 
-setwd("~/Dropbox/nceas_kelp_climate_2013/temporal_change/github_repo/03_derived_data")
-rw<-read.csv("CleanDataWithRegions.csv")
+rw<-read.csv("../03_derived_data/CleanDataWithRegions.csv")
 rw$uniq<-paste(rw$year, rw$Site, sep="_")
+rw$StudySite <- paste(rw$Study, rw$Site, rw$trajectory_ID,sep="-")
 year<-rw[,c("year", "uniq")]
 sw<-unique(year)
 y<-table(sw$year)
-yc<-cbind(y)
-
-pdf(width= 7,height= 4,"n_by_year.pdf")
+yc<-data.frame(cbind(y))
+yc$year <- row.names(yc)
+  
+pdf(width= 7,height= 4,"../Figures/n_by_year.pdf")
 par(mar=c(5.1,5,4.1,2.1))
-plot(y~row.names, data=yc)
+plot(y~year, data=yc)
 barplot(y, xlab="Year", ylab="Number of Sites", ylim=c(0,800), cex.lab=1.5)
 dev.off()
 
@@ -19,7 +20,7 @@ dev.off()
 
 sites_year<-sw %>% 
   group_by(year) %>% 
-  summarise(count=sum(id))
+  summarise(count=length(id))
 
 library(plyr)
 
@@ -28,6 +29,6 @@ s_n<-unique(s_n)
 site.freq<-table(s_n$year)
 si_f<-cbind(site.freq)
 
-pdf(width= 7,height= 4,"sites_by_year.pdf")
+pdf(width= 7,height= 4,"../Figures/sites_by_year.pdf")
 barplot(site.freq, xlab="Year", ylab="Number of Sites", cex.lab=1.5)
 dev.off()
