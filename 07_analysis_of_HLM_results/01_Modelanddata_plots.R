bd65bc149a2544c4bd8ecb5829dbbe2adbfc727b
Dan Okamoto <okamoto@aurora>
2016-02-02 07:52:11 -0800

    Added log-scale plots of new model runs

--- 07_analysis_of_HLM_results/01_Modelanddata_plots.R
+++ 07_analysis_of_HLM_results/01_Modelanddata_plots.R
@@ -1,7 +1,7 @@
-setwd("~/Dropbox/nceas_kelp_climate_2013/temporal_change/github_repo/06_hlm_output")
+setwd("~/Dropbox/nceas_kelp_climate_2013/temporal_change/github_repo/06_HLM_output")
 
-load("../06_hlm_output/combined_model_summaries.RData")
-#load("../06_hlm_output/model_summaries1900-2015_2015-11-02.RData")
+load("../06_HLM_output/combined_model_summaries2003-2012_2016-02-01.RData")
+load("../1900-2015_2016-02-01.RData")
 
 library(rstan);library(plyr);library(gdata);library(ggplot2)
 library(parallel);library(grid);library(coda)
@@ -21,15 +21,18 @@ for (j in 1:4){
                      legend.background = element_rect(colour = "NA",fill = NA, size = .25),
                      legend.position = c(.2,0.9),
                      axis.ticks.length = unit(-0.25,"lines"),
-                     axis.ticks.margin = unit(0.5,"lines"),
                      panel.background=element_rect(fill= NA),
                      legend.direction="horizontal",
                      legend.title = element_text(size = 0),
                      panel.border= element_rect(fill= NA),
                      legend.key.width = unit(2,"lines"),
                      plot.margin = unit(rep(1, 4), "inches"))
-   model_results$pred <-  exp(colMeans(model_list[[1]]$chains$y_loc))
-  model_results$pred2 <-  exp(colMeans(model_list[[1]]$chains$y_loc_mu))
+    
+  model_results$pred <-  exp(colMeans(model_list[[j]]$chains$y_loc))
+  model_results$pred2 <-  exp(colMeans(model_list[[j]]$chains$y_loc_mu))
+  model_results$pred_CIU <-  exp(apply(model_list[[j]]$chains$y_loc_mu,2,quantile,probs= 0.975))
+  model_results$pred_CIL <-  exp(apply(model_list[[j]]$chains$y_loc_mu,2,quantile,probs= 0.025))
+  
     pdf(width= 6, height= 6,file= paste("../Figures/full_dataset_predictions_",  grouping_list[j],".pdf",sep= ""))
       for (i in 1:length(unique(model_results$group_name))){
         data <- subset(model_results,group_name==unique(model_results$group_name)[i])
@@ -38,6 +41,7 @@ for (j in 1:4){
       
         p_val <- ifelse(params$p[params$parameter=="mean_slope"]>0.95|params$p[params$parameter=="mean_slope"]<0.05,1,3)
         plot1 <- ggplot(aes(x=Year),data=data)+
+          geom_ribbon(aes(y=pred2,ymax= pred_CIU,ymin= pred_CIL),fill= "grey90")+
           geom_point(aes(y=y,colour= SiteName,group= SiteName),alpha= 0.5,size= 2)+
           geom_line(aes(y=pred,colour= SiteName,group= SiteName))+
           geom_line(aes(y=y,colour= SiteName,group= SiteName),linetype= 3,size=0.1)+theme_bw()+
@@ -45,11 +49,36 @@ for (j in 1:4){
           geom_line(aes(y=pred2),linetype= p_val)+
           ylab("standardized kelp + 0.001")+
           xlab("Year")+
-          coord_cartesian(ylim= c(0,max))+
+          coord_cartesian(ylim= c(0.001,max))+
           ggtitle(paste(unique(model_results$group_name)[i],  grouping_list[j],sep= ";")) 
         print(plot1)
       }
     dev.off()
+
+pdf(width= 6, height= 6,file= paste("../Figures/full_dataset_predictions_log",  grouping_list[j],".pdf",sep= ""))
+for (i in 1:length(unique(model_results$group_name))){
+  data <- subset(model_results,group_name==unique(model_results$group_name)[i])
+  max <- data$y*1.2
+  params <- subset(combined_summaries,group_name==unique(model_results$group_name)[i]&parameter%in%c("mean_slope","mean_intercept")&Period=="1900-2015"&grouping==grouping_list[j])[,c("mean","parameter","p")]
+  
+  p_val <- ifelse(params$p[params$parameter=="mean_slope"]>0.95|params$p[params$parameter=="mean_slope"]<0.05,1,3)
+  plot1 <- ggplot(aes(x=Year),data=data)+
+    geom_ribbon(aes(y=pred2,ymax= pred_CIU,ymin= pred_CIL),fill= "grey90")+
+    geom_point(aes(y=y,colour= SiteName,group= SiteName),alpha= 0.5,size= 2)+
+    geom_line(aes(y=pred,colour= SiteName,group= SiteName))+
+    geom_line(aes(y=y,colour= SiteName,group= SiteName),linetype= 3,size=0.1)+theme_bw()+
+    theme(legend.position="none")+
+    geom_line(aes(y=pred2),linetype= p_val)+
+    ylab("standardized kelp + 0.001")+
+    scale_y_continuous(trans= "log10",labels = trans_format("log10", math_format(10^.x)),expand= c(0,0))+
+    annotation_logticks(base=10,sides= "l")+
+    xlab("Year")+
+    coord_cartesian(ylim= c(0.0009,max*1.05))+
+    ggtitle(paste(unique(model_results$group_name)[i],  grouping_list[j],sep= ";")) 
+  print(plot1)
 }
+dev.off()
+}
+
 write.csv(combined_summaries,"../06_HLM_output/site_slopes.csv",row.names= F)
 
