e5fd66a7a5049aaac167a11cff3c01057f957c9f
dkokamoto <d.okamoto@sfu.ca>
2016-03-15 12:45:34 -0700

    updated plots for thicker lines and larger points

--- 05_HLM_analysis_code/plot_funs.R
+++ 05_HLM_analysis_code/plot_funs.R
@@ -66,20 +66,19 @@ plot_fun_1 <- function(ident,quant_method= "direct") {
   ### generate plots (original and log-scale)
   plot1 <- ggplot(aes(x=Year),data=data)+
     geom_ribbon(aes(x=Year,y= pred,ymax= CIU,ymin= CIL),
-             data= newdata,fill="grey50",alpha=0.5)+
-    geom_point(aes(y=y,colour= SiteName,shape= StudyName),alpha= 0.5,size= 1.5)+
-    geom_line(aes(y=y,colour= SiteName),size=0.1,linetype=3)+
-    geom_line(aes(y=pred,colour= SiteName,linetype= signif),alpha= 0.5,size=0.5)+
-   
+                data= newdata,fill="grey50",alpha=0.5)+
+    geom_point(aes(y=y,colour= SiteName,shape= StudyName),alpha= 0.5,size= 3)+
+    geom_line(aes(y=pred,colour= SiteName,linetype= signif),alpha= 0.5)+
+    geom_line(aes(y=y,colour= SiteName),linetype= 3,size=0.1)+
     geom_line(aes(y=pred,x= Year),data= newdata,
-              linetype= p_val,colour= "black",size=0.8)+
-    ylab("Standardized Kelp Abundance+0.01")+
+              linetype= p_val,colour= "black",size=1)+
+    ylab("Standardized Kelp Abundance+0.001")+
     options+
     xlab("Year")+
     scale_y_continuous(breaks= c(0,0.5,1.0,1.5),labels= c(0,0.5,1.0,1.5))+
     scale_shape_manual(values= c(16:17,3,4,7,8,9,12,13)[(1:length(unique(data$Study)))])+
-    scale_linetype_manual(values= c(2,1))+
     guides(colour= FALSE,linetype= FALSE,shape= guide_legend(title="Study" ,title.hjust= 0.5,ncol= 2))+
+    scale_linetype_manual(values= c(2,1))+
     coord_cartesian(ylim= c(-0.01,maxp),expand= c(0,0))+
     annotate("text",  x=Inf, y = Inf, label = slope, vjust=1.5, hjust=1.05)+
     ggtitle(title)
@@ -88,7 +87,6 @@ plot_fun_1 <- function(ident,quant_method= "direct") {
     scale_y_continuous(trans= "log10",breaks= c(0,0.001,0.01,0.1,0.5,1.0),labels= c(0,0.001,0.01,0.1,0.5,1.0),expand= c(0,0))+
     annotation_logticks(base=10,sides= "l")+
     coord_cartesian(ylim= c(minp,maxp))
-  
   plot3 <- ggplot(aes(x=Year_group_cent),data=data)+
     geom_ribbon(aes(x=Year_group_cent,ymax= CIU_cent,ymin= CIL_cent),
                 data= newdata,fill="grey50",alpha=0.5)+
@@ -126,7 +124,7 @@ plot_fun_2 <- function(ident,quant_method= "direct") {
   newdata <- data.frame(list(x= seq(from= min(data$x),to= max(data$x),length.out= 40),
                              Year_group_cent= seq(from= min(data$Year_group_cent),to= max(data$Year_group_cent),length.out= 40)))
   
-  ### generate full range of ecoregion level predictions ###
+  ### generate full range of group level predictions ###
   pred_fun <- function(x) model_list[[j]]$chains$beta_mu[,ident,1]+model_list[[j]]$chains$beta_mu[,ident,2]*x
   pred_fun2 <- function(x) model_list[[j]]$chains$beta_mu[,ident,2]*x
   pred <- data.frame(mapply(pred_fun,newdata$x))
@@ -190,14 +188,14 @@ plot_fun_2 <- function(ident,quant_method= "direct") {
   ### generate plots (original and log-scale)
   plot1 <- ggplot(aes(x=Year),data=data)+
     geom_ribbon(aes(x=Year,y= pred,ymax= CIU,ymin= CIL),
-             data= newdata,fill="grey50",alpha=0.5)+
-    geom_point(aes(y=y,fill= SiteName),alpha= 0.5,size= 0.5,shape= 21)+
-    geom_line(aes(y=y,colour= SiteName),size=0.1,linetype=3)+
-    geom_line(aes(y=pred,colour= SiteName,linetype= signif),size= 0.5,alpha= 0.5)+
+                data= newdata,fill="grey50",alpha=0.5)+
+    geom_point(aes(y=y,fill= SiteName),alpha= 0.5,size= 0.75,shape= 21)+
+    geom_line(aes(y=pred,colour= SiteName,linetype= signif),alpha= 0.5)+
+    geom_line(aes(y=y,colour= SiteName),linetype= 3,size=0.1)+
     geom_line(aes(y=pred,x= Year),data= newdata, 
               linetype= p_val,colour= "black",size=1)+
     scale_y_continuous(breaks= c(0,0.5,1.0,1.5),labels= c(0,0.5,1.0,1.5))+
-    ylab("Standardized Kelp Abundance+0.01")+
+    ylab("Standardized Kelp Abundance+0.001")+
     options+
     xlab("Year")+
     scale_linetype_manual(values= c(2,1))+
@@ -207,10 +205,9 @@ plot_fun_2 <- function(ident,quant_method= "direct") {
     ggtitle(title)
   
   plot2 <- plot1+
-  scale_y_continuous(trans= "log10",breaks= c(0,0.001,0.01,0.1,0.5,1.0),labels= c(0,0.001,0.01,0.1,0.5,1.0),expand= c(0,0))+
+    scale_y_continuous(trans= "log10",breaks= c(0,0.001,0.01,0.1,0.5,1.0),labels= c(0,0.001,0.01,0.1,0.5,1.0),expand= c(0,0))+
     annotation_logticks(base=10,sides= "l")+
     coord_cartesian(ylim= c(minp,maxp))
-  
   plot3 <-  ggplot(aes(x=Year_group_cent),data=data)+
     geom_ribbon(aes(x=Year_group_cent,ymax= CIU_cent,ymin= CIL_cent),
                 data= newdata,fill="grey50",alpha=0.5)+
